77b41d2e3f76dbea0772d3a8f470ee3c5ef52a51
mytypy <bletigri@gmail.com>
2025-07-05 17:11:59 +0300

    make more

--- auth/auth.py
+++ auth/auth.py
@@ -1,4 +1,34 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
+from .auth_conf import UserAuth
 
 router = APIRouter(prefix='/auth', tags=['Работа с авторизации'])
+
+
+class InitData(BaseModel):
+    initData: str
+        
+
+@router.post('/login')
+async def login_user(data: InitData):
+    auth = UserAuth()
+    
+    verify = auth.telegram_validate(init_data=data.model_dump())
+    
+    if verify:
+        return {'response': 'meow'}
+    
+    return {'response': 'nooo'}
+
+
+@router.post('/register')
+async def login_user(data: InitData):
+    auth = UserAuth()
+    
+    verify = auth.telegram_validate(init_data=data.model_dump())
+    
+    if verify:
+        return {'response': 'meow'}
+    
+    return {'response': 'nooo'}
